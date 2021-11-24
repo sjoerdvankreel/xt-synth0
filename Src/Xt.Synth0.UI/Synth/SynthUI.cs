2a0b90ed9e6b7d5775bb8338a512eda1b5824be9
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2021-11-24 16:23:28 +0100

    take 34

--- Src/Xt.Synth0.UI/Synth/SynthUI.cs
+++ Src/Xt.Synth0.UI/Synth/SynthUI.cs
@@ -8,14 +8,14 @@ namespace Xt.Synth0.UI
 		public static UIElement Make(SynthModel model)
 		{
 			var result = UI.MakeGrid(4, 3);
-			int halfLength = PatternModel.Length / 2;
 			result.Margin = new(UI.Margin);
+			int rows = PatternModel.Length / 2;
 			result.Children.Add(GroupUI.Make(model.Unit1, "Unit1", new(0, 0)));
 			result.Children.Add(GroupUI.Make(model.Unit2, "Unit2", new(1, 0)));
 			result.Children.Add(GroupUI.Make(model.Unit3, "Unit3", new(2, 0)));
 			result.Children.Add(GroupUI.Make(model.Global, "Global", new(3, 0)));
-			result.Children.Add(PatternUI.Make(model.Pattern, "Pattern1", 0, halfLength, new(0, 1, 4)));
-			result.Children.Add(PatternUI.Make(model.Pattern, "Pattern2", halfLength, halfLength, new(0, 2, 4)));
+			result.Children.Add(PatternUI.Make(model.Pattern, "Pattern1", 0, rows, new(0, 1, 4)));
+			result.Children.Add(PatternUI.Make(model.Pattern, "Pattern2", rows, rows, new(0, 2, 4)));
 			return result;
 		}
 	}
