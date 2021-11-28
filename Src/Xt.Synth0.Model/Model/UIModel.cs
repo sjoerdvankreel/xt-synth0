579ca67452c93d14902539422194ff221f988111
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2021-11-28 20:19:38 +0100

    take 120

---
+++ Src/Xt.Synth0.Model/Model/UIModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Xt.Synth0.Model
+{
+	public class UIModel : INotifyPropertyChanged
+	{
+		public event EventHandler StopRequest;
+		public event EventHandler StartRequest;
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		ThemeType _theme;
+		public ThemeType Theme
+		{
+			get => _theme;
+			set => Set(ref _theme, value);
+		}
+
+		bool _isRunning;
+		public bool IsRunning
+		{
+			get => _isRunning;
+			set => Set(ref _isRunning, value);
+		}
+
+		public void RequestStop() => StopRequest?.Invoke(this, EventArgs.Empty);
+		public void RequestStart() => StartRequest?.Invoke(this, EventArgs.Empty);
+
+		void Set<T>(ref T field, T value, [CallerMemberName] string property = null)
+		{
+			field = value;
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+		}
+	}
+}
