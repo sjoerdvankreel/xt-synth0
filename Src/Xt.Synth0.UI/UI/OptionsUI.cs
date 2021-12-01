d059e5777cfddf443d3f8c42cd91829f5337cba8
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2021-12-01 17:21:14 +0100

    take 150

--- Src/Xt.Synth0.UI/UI/OptionsUI.cs
+++ Src/Xt.Synth0.UI/UI/OptionsUI.cs
@@ -12,8 +12,17 @@ namespace Xt.Synth0.UI
 			OptionsModel options, AudioModel audio)
 		{
 			var window = Create.Window(options);
-			window.Content = MakeGroup(window, options, audio);
+			window.Content = MakeContent(window, options, audio);
 			window.ShowDialog();
+		}
+
+		static UIElement MakeContent(
+			Window window, OptionsModel options, AudioModel audio)
+		{
+			var result = new StackPanel();
+			result.Children.Add(MakeGroup(window, options, audio));
+			result.Children.Add(MakeOK(window));
+			return result;
 		}
 
 		static UIElement MakeGroup(
@@ -21,14 +30,14 @@ namespace Xt.Synth0.UI
 		{
 			var result = new GroupBox();
 			result.Header = $"Options";
-			result.Content = MakeContent(window, options, audio);
+			result.Content = MakeGrid(window, options, audio);
 			return result;
 		}
 
-		static UIElement MakeContent(
+		static UIElement MakeGrid(
 			Window window, OptionsModel options, AudioModel audio)
 		{
-			var result = Create.Grid(5, 2);
+			var result = Create.Grid(4, 2);
 			result.Children.Add(Create.Label("Theme", new(0, 0)));
 			result.Children.Add(MakeTheme(options, new(0, 1)));
 			result.Children.Add(Create.Label("Use ASIO", new(1, 0)));
@@ -38,14 +47,22 @@ namespace Xt.Synth0.UI
 			result.Children.Add(MakeWasapiDevice(options, audio, new(2, 1)));
 			result.Children.Add(Create.Label("Sample rate", new(3, 0)));
 			result.Children.Add(MakeSampleRate(options, new(3, 1)));
-			result.Children.Add(MakeOK(window, new(4, 1)));
+			return result;
+		}
+
+		static UIElement MakeOK(Window window)
+		{
+			var result = new Button();
+			result.Content = "OK";
+			result.Click += (s, e) => window.Close();
+			result.HorizontalAlignment = HorizontalAlignment.Right;
 			return result;
 		}
 
 		static UIElement MakeAsioDevice(
 			OptionsModel options, AudioModel audio, Cell cell)
 		{
-			var result = MakeDeviceDevice(options, true,
+			var result = MakeDevice(options, true,
 				nameof(OptionsModel.AsioDeviceId), cell);
 			result.ItemsSource = audio.AsioDevices;
 			return result;
@@ -54,18 +71,9 @@ namespace Xt.Synth0.UI
 		static UIElement MakeWasapiDevice(
 			OptionsModel options, AudioModel audio, Cell cell)
 		{
-			var result = MakeDeviceDevice(options, false,
+			var result = MakeDevice(options, false,
 				nameof(OptionsModel.WasapiDeviceId), cell);
 			result.ItemsSource = audio.WasapiDevices;
-			return result;
-		}
-
-		static UIElement MakeOK(Window window, Cell cell)
-		{
-			var result = Create.Element<Button>(cell);
-			result.Content = "OK";
-			result.Click += (s, e) => window.Close();
-			result.HorizontalAlignment = HorizontalAlignment.Right;
 			return result;
 		}
 
@@ -95,7 +103,7 @@ namespace Xt.Synth0.UI
 			return result;
 		}
 
-		static ComboBox MakeDeviceDevice(
+		static ComboBox MakeDevice(
 			OptionsModel options, bool asio, string path, Cell cell)
 		{
 			var result = Create.Element<ComboBox>(cell);
