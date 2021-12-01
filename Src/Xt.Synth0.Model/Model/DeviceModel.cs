b0240f01948a42a2d9d9463722d8816f0f2da163
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2021-12-01 18:39:54 +0100

    take 156

--- Src/Xt.Synth0.Model/Model/DeviceModel.cs
+++ Src/Xt.Synth0.Model/Model/DeviceModel.cs
@@ -1,19 +1,8 @@
 ﻿namespace Xt.Synth0.Model
 {
-	public sealed class DeviceModel : ViewModel
+	public sealed class DeviceModel
 	{
-		string _id;
-		public string Id
-		{
-			get => _id;
-			set => Set(ref _id, value);
-		}
-
-		string _name;
-		public string Name
-		{
-			get => _name;
-			set => Set(ref _name, value);
-		}
+		public string Id { get; set; }
+		public string Name { get; set; }
 	}
 }
