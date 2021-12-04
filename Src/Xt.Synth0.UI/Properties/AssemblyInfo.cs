c06cf7aa2b648b084ca2f31dbe64ddcd15e1b2b9
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2021-12-04 19:29:46 +0100

    take 168

--- Src/Xt.Synth0.UI/Properties/AssemblyInfo.cs
+++ Src/Xt.Synth0.UI/Properties/AssemblyInfo.cs
@@ -1,2 +1,5 @@
 using System.Windows;
+using System.Reflection;
+
+[assembly: AssemblyTitle("Xt.Synth0.UI")]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
