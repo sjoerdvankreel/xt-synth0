2cb3645c520180b27f4f46a6f7f0f281f25488ee
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2021-12-05 19:15:29 +0100

    take 192

--- Src/Xt.Synth0.DSP/SynthDSP.cs
+++ Src/Xt.Synth0.DSP/SynthDSP.cs
@@ -5,20 +5,50 @@ namespace Xt.Synth0.DSP
 {
 	public class SynthDSP
 	{
-		float _phase;
+		readonly float[] _phases = new float[3];
+		readonly UnitModel[] _units = new UnitModel[3];
 
 		public void Next(SynthModel model, float rate, float[] buffer, int frames)
 		{
-			UnitModel unit = model.Unit1;
-			float midi = (unit.Oct.Value + 1) * 12 + unit.Note.Value + unit.Cent.Value / 100.0f;
-			float freq = 440.0f * MathF.Pow(2.0f, (midi - 69.0f) / 12.0f);
-			for (int i = 0; i < frames; i++)
+			_units[0] = model.Unit1;
+			_units[1] = model.Unit2;
+			_units[2] = model.Unit3;
+			for (int i = 0; i < frames * 2; i++)
 			{
-				var sample = (float)Math.Sin(_phase * Math.PI * 2.0f);
-				buffer[i * 2] = sample;
-				buffer[i * 2 + 1] = sample;
-				_phase += freq / rate;
-				if (_phase >= 1.0f) _phase = -1.0f;
+				buffer[i] = 0;
+			}
+			float synAmp = model.Amp.Lvl.Value / 255f;
+			for (int u = 0; u < 3; u++)
+			{
+				UnitModel unit = _units[u];
+				if (unit.On.Value == 0) continue;
+				float midi = (unit.Oct.Value + 1) * 12 + unit.Note.Value + unit.Cent.Value / 100.0f;
+				float freq = 440.0f * MathF.Pow(2.0f, (midi - 69.0f) / 12.0f);
+				float unitAmp = unit.Amp.Value / 255f;
+				for (int f = 0; f < frames; f++)
+				{
+					float sample = 0;
+					switch (unit.Type.Value)
+					{
+						case (int)UnitType.Sin:
+							sample = (float)Math.Sin(_phases[u] * Math.PI * 2.0f);
+							break;
+						case (int)UnitType.Saw:
+							sample = _phases[u] * 2 - 1;
+							break;
+						case (int)UnitType.Tri:
+							sample = _phases[u] * 2 - 1;
+							break;
+						case (int)UnitType.Sqr:
+							sample = _phases[u] > 0.5f ? 1 : -1;
+							break;
+					}
+
+					buffer[f * 2] += sample * unitAmp * synAmp;
+					buffer[f * 2 + 1] += sample * unitAmp * synAmp;
+					_phases[u] += freq / rate;
+					if (_phases[u] >= 1.0f) _phases[u] = -1.0f;
+				}
 			}
 		}
 	}
