0f6fe990da77353e8275356f33d69be906d8de73
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2021-12-07 22:56:26 +0100

    take 200

--- Src/Xt.Synth0.Model/Synth/Pattern/PatternRow.cs
+++ Src/Xt.Synth0.Model/Synth/Pattern/PatternRow.cs
@@ -6,8 +6,8 @@ namespace Xt.Synth0.Model
 {
 	public sealed class PatternRow : SubModel
 	{
-		public const int MaxFxCount = 3;
-		public const int MaxKeyCount = 5;
+		public const int MaxFxCount = 2;
+		public const int MaxKeyCount = 3;
 
 		static IEnumerable<PatternFx> MakeFx()
 		=> Enumerable.Repeat(0, MaxFxCount).Select(_ => new PatternFx());
--- Src/Xt.Synth0.UI/Pattern/PatternUI.cs
+++ Src/Xt.Synth0.UI/Pattern/PatternUI.cs
@@ -36,9 +36,11 @@ namespace Xt.Synth0.UI
 		{
 			var track = model.Track;
 			var pattern = model.Pattern;
+			var fx = PatternRow.MaxFxCount;
+			var keys = PatternRow.MaxKeyCount;
 			var rows = PatternModel.PatternRows;
 			var offset = index * PatternModel.PatternRows;
-			var result = Create.Grid(rows, 20);
+			var result = Create.Grid(rows, keys * 5 + fx * 3);
 			for (int r = 0; r < rows; r++)
 				AddRow(result, track, pattern.Rows[offset + r], r);
 			return result;
@@ -47,24 +49,26 @@ namespace Xt.Synth0.UI
 		static void AddRow(Grid grid, TrackModel track, PatternRow row, int r)
 		{
 			AddKeys(grid, track, row, r);
-			AddFx(grid, track, row, r);	
-		}
-
-		static void AddKeys(Grid grid, TrackModel track, PatternRow row, int r)
-		{
-			PatternKeyUI.Add(grid, row.Keys[0], track, 1, r, 0);
-			grid.Children.Add(Create.Divider(new(r, 4), track.Keys, 2));
-			PatternKeyUI.Add(grid, row.Keys[1], track, 2, r, 5);
-			grid.Children.Add(Create.Divider(new(r, 9), track.Keys, 3));
-			PatternKeyUI.Add(grid, row.Keys[2], track, 3, r, 10);
+			AddFx(grid, track, row, r);
 		}
 
 		static void AddFx(Grid grid, TrackModel track, PatternRow row, int r)
 		{
-			grid.Children.Add(Create.Divider(new(r, 14), track.Fx, 1));
-			PatternFxUI.Add(grid, row.Fx[0], track.Fx, 1, r, 15);
-			grid.Children.Add(Create.Divider(new(r, 17), track.Fx, 2));
-			PatternFxUI.Add(grid, row.Fx[1], track.Fx, 2, r, 18);
+			int startCol = PatternRow.MaxKeyCount * 5;
+			for (int f = 0; f < PatternRow.MaxFxCount; f++)
+			{
+				grid.Children.Add(Create.Divider(new(r, startCol + f * 3), track.Fx, f + 1));
+				PatternFxUI.Add(grid, row.Fx[f], track.Fx, f + 1, r, startCol + 1 + f * 3);
+			}
+		}
+
+		static void AddKeys(Grid grid, TrackModel track, PatternRow row, int r)
+		{
+			for (int k = 0; k < PatternRow.MaxKeyCount; k++)
+			{
+				PatternKeyUI.Add(grid, row.Keys[k], track, k + 1, r, k * 5);
+				grid.Children.Add(Create.Divider(new(r, (k + 1) * 5 - 1), track.Keys, k + 1));
+			}
 		}
 	}
 }
