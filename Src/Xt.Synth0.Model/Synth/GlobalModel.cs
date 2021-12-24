3a9942a6ce78a7eeff3e986eb1ae601cb28b5102
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2021-12-24 19:49:00 +0100

    take 357

--- Src/Xt.Synth0.Model/Synth/GlobalModel.cs
+++ Src/Xt.Synth0.Model/Synth/GlobalModel.cs
@@ -1,13 +1,22 @@
-﻿namespace Xt.Synth0.Model
+﻿using System;
+using System.Linq;
+
+namespace Xt.Synth0.Model
 {
 	public sealed class GlobalModel : GroupModel
 	{
+		public static readonly string[] Methods = Enum.
+			GetValues<SynthMethod>().Select(v => v.ToString()).ToArray();
+
 		static readonly ParamInfo BpmInfo = new DiscreteInfo(
 			nameof(Bpm), "Tempo", 1, 255, 120);
+		static readonly ParamInfo MethodInfo = new EnumInfo<SynthMethod>(
+			nameof(Method), "Synthesis method", Methods);
 		public Param Bpm { get; } = new(BpmInfo);
+		public Param Method { get; } = new(MethodInfo);
 
 		internal GlobalModel(string name) : base(name) { }
 		internal override Param[][] ListParamGroups() 
-		=> new[] { new[] { Bpm } };
+		=> new[] { new[] { Bpm, Method } };
 	}
 }
