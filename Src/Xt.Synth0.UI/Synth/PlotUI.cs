6fa9f1959295a42bba35842d8940c7b8340923f3
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2021-12-25 01:06:55 +0100

    take 373

--- Src/Xt.Synth0.UI/Synth/PlotUI.cs
+++ Src/Xt.Synth0.UI/Synth/PlotUI.cs
@@ -9,26 +9,25 @@ namespace Xt.Synth0.UI
 {
 	public static class PlotUI
 	{
-		const int Width = 100;
-		const int Height = 50;
+		const int MinHeight = 24;
 		public static event EventHandler<RequestPlotDataEventArgs> RequestPlotData;
 
 		internal static UIElement Make(AppModel model)
 		{
 			var result = new GroupBox();
 			result.Header = "Plot";
-			result.Content = Plot();
-			model.Synth.ParamChanged += (s, e) => result.Content = Plot();
-			model.Settings.PropertyChanged += (s, e) => result.Content = Plot();
+			result.Content = MakeContent(model);
 			return result;
 		}
 
-		static UIElement Plot()
+		static UIElement MakeContent(AppModel model)
 		{
-			var result = new Canvas();
-			result.Width = Width;
-			result.Height = Height;
-			result.Children.Add(PlotLine());
+			var result = new ContentControl();
+			result.MinHeight = MinHeight;
+			result.Content = Plot(result);
+			result.SizeChanged += (s, e) => result.Content = Plot(result);
+			model.Synth.ParamChanged += (s, e) => result.Content = Plot(result);
+			model.Settings.PropertyChanged += (s, e) => result.Content = Plot(result);
 			return result;
 		}
 
@@ -39,22 +38,31 @@ namespace Xt.Synth0.UI
 			return args.Data;
 		}
 
-		static UIElement PlotLine()
+		static UIElement Plot(FrameworkElement container)
+		{
+			var result = new Canvas();
+			result.Width = container.ActualWidth;
+			result.Height = container.ActualHeight;
+			result.Children.Add(PlotLine(container));
+			return result;
+		}
+
+		static UIElement PlotLine(FrameworkElement container)
 		{
 			var result = new Polyline();
 			result.StrokeThickness = 1;
 			result.Stroke = Brushes.White;
-			result.Points = MapPlotData(GetPlotData());
+			result.Points = MapPlotData(container, GetPlotData());
 			return result;
 		}
 
-		static PointCollection MapPlotData(float[] data)
+		static PointCollection MapPlotData(FrameworkElement container, float[] data)
 		{
 			var result = new PointCollection();
 			for (int i = 0; i < data.Length; i++)
 			{
-				var x = (double)i / data.Length * Width;
-				var y = (-data[i] * 0.5 + 0.5) * Height;
+				var x = (double)i / data.Length * container.ActualWidth;
+				var y = (-data[i] * 0.5 + 0.5) * container.ActualHeight;
 				result.Add(new Point(x, y));
 			}
 			return result;
