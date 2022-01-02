5a945ed7f7d245d4c75f886872d6416ea1bc038e
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2022-01-02 14:53:42 +0100

    temp? take 494

--- Src/Xt.Synth0.DSP/DSP/PatternDSP.cpp
+++ Src/Xt.Synth0.DSP/DSP/PatternDSP.cpp
@@ -1,4 +1,5 @@
 #include "PatternDSP.hpp"
+#include <cassert>
 
 namespace Xts {
 
@@ -12,11 +13,13 @@ PatternDSP::Automate(EditModel const& edit, PatternRow const& row, SynthModel& s
 void 
 PatternDSP::Automate(PatternFx const& fx, SynthModel& synth) const
 {
-		if (fx.target == 0 || fx.target > TrackConstants::ParamCount) return;
-    auto const& p = synth.params[fx.target - 1];
-		if (fx.value < p.min) *p.value = p.min;
-		else if (fx.value > p.max) *p.value = p.max;
-		else *p.value = fx.value;
+  assert(fx.value >= 0);
+  assert(fx.target >= 0);
+  if (fx.target == 0 || fx.target > TrackConstants::ParamCount) return;
+  auto const& p = synth.params[fx.target - 1];
+  if (fx.value < p.min) *p.value = p.min;
+  else if (fx.value > p.max) *p.value = p.max;
+  else *p.value = fx.value;
 }
 
 } // namespace Xts
