26551619f13fb6285e2d9a0c427fdefcd2111ae4
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2022-01-08 12:57:22 +0100

    take 565

--- Src/Xt.Synth0.UI/Converters/ShowRowConverter.cs
+++ Src/Xt.Synth0.UI/Converters/ShowRowConverter.cs
@@ -8,6 +8,6 @@ namespace Xt.Synth0.UI
 		readonly int _min;
 		internal ShowRowConverter(int row, int min) => (_row, _min) = (row, min);
 		protected override Visibility Convert(int rows, int param)
-		=> _row >= rows - 1 && param >= _min ? Visibility.Visible : Visibility.Collapsed;
+		=> _row < rows && param >= _min ? Visibility.Visible : Visibility.Collapsed;
 	}
 }
