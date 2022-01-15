c428eeba6ba098f6c4a47e0d027d369ebfcc8951
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2022-01-15 21:35:33 +0100

    take 656

--- Src/Xt.Synth0.DSP/DSP/SequencerDSP.hpp
+++ Src/Xt.Synth0.DSP/DSP/SequencerDSP.hpp
@@ -9,25 +9,29 @@
 
 namespace Xts {
 
+struct SequencerOutput
+{
+  int32_t currentRow;
+  int64_t streamPosition;
+};
+
 class SequencerDSP
 {
+  SynthDSP _synth;
+  PatternDSP const _pattern;
+
   int _currentRow = 0;
 	int _previousRow = -1;
   double _rowFactor = 0.0;
   uint64_t _streamPosition = 0;
 
-  SynthDSP _synth;
-  PatternDSP const _pattern;
-
   bool RowUpdated();
   bool UpdateRow(SequencerModel const& seq, SynthModel& synth, float rate);
-  void Next(SequencerModel const& seq, SynthModel& synth, float rate, float* l, float* r);
+  void Next(SequencerModel const& seq, SynthModel& synth, float rate, SynthOutput& output);
 
 public:
-  void Reset();
-  void ProcessBuffer(
-    SequencerModel const& seq, SynthModel& synth, float rate, 
-    float* buffer, int32_t frames, int32_t* currentRow, int64_t* streamPosition);
+  void Init();
+  void ProcessBuffer(SequencerModel const& seq, SynthModel& synth, float rate, float* buffer, int32_t frames, SequencerOutput& output);
 };
 
 } // namespace Xts
