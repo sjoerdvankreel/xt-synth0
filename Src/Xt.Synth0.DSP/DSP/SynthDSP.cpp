0d8147959508440c79ba94a8658646f95f63e434
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2022-01-15 21:31:27 +0100

    take 655

--- Src/Xt.Synth0.DSP/DSP/SynthDSP.cpp
+++ Src/Xt.Synth0.DSP/DSP/SynthDSP.cpp
@@ -1,29 +1,36 @@
 #include "SynthDSP.hpp"
 #include "DSP.hpp"
+#include <cstring>
 
 namespace Xts {
 
 void
-SynthDSP::Reset()
+SynthDSP::Release()
 {
-  for(int u = 0; u < TrackConstants::UnitCount; u++)
-    _units[u].Reset();
+  for (int e = 0; e < TrackConstants::EnvCount; e++)
+    _envs[e].Release();
 }
 
 void
-SynthDSP::Next(SynthModel const& synth, float rate, int octave, PatternNote note, bool tick, float* l, float* r)
+SynthDSP::Init(int oct, UnitNote note)
 {
-  float ul;
-  float ur;
-  *l = 0.0f;
-  *r = 0.0f;
-  bool cycled = false;
+  for(int e = 0; e < TrackConstants::EnvCount; e++)
+    _envs[e].Init();
+  for(int u = 0; u < TrackConstants::UnitCount; u++)
+    _units[u].Init(oct, note);
+}
+
+void
+SynthDSP::Next(SynthModel const& synth, float rate, SynthOutput& output)
+{
+  UnitOutput uout;
+  memset(&output, 0, sizeof(output));
   float amp = Level(synth.global.amp);
   for (int u = 0; u < TrackConstants::UnitCount; u++)
   {
-    _units[u].Next(synth.units[u], rate, octave, note, tick, false, &ul, &ur, &cycled);
-    *l += ul * amp;
-    *r += ur * amp;
+    _units[u].Next(synth.units[u], rate, uout);
+    output.l += uout.l * amp;
+    output.r += uout.r * amp;
   }
 }
 
--- Src/Xt.Synth0.DSP/DSP/SynthDSP.hpp
+++ Src/Xt.Synth0.DSP/DSP/SynthDSP.hpp
@@ -1,18 +1,27 @@
 #ifndef XTS_SYNTH_DSP_HPP
 #define XTS_SYNTH_DSP_HPP
 
+#include "EnvDSP.hpp"
 #include "UnitDSP.hpp"
 #include "../Model/SynthModel.hpp"
-#include "../Model/SequencerModel.hpp"
 
 namespace Xts {
 
+struct SynthOutput
+{
+  float l;
+  float r;
+};
+
 class SynthDSP
 {
+  EnvDSP _envs[TrackConstants::EnvCount];
   UnitDSP _units[TrackConstants::UnitCount];
+
 public:
-  void Reset();
-  void Next(SynthModel const& synth, float rate, int octave, PatternNote note, bool tick, float* l, float* r);
+  void Release();
+  void Init(int oct, UnitNote note);
+  void Next(SynthModel const& synth, float rate, SynthOutput& output);
 };
 
 } // namespace Xts
