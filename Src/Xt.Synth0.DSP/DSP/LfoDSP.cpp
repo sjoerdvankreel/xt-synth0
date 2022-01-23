bca6b859c29cc10cf9e4fddc76f4ae614c1da78d
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2022-01-23 14:42:12 +0100

    take 800

--- Src/Xt.Synth0.DSP/DSP/LfoDSP.cpp
+++ Src/Xt.Synth0.DSP/DSP/LfoDSP.cpp
@@ -1,13 +1,9 @@
 #include "DSP.hpp"
 #include "LfoDSP.hpp"
+#include <cmath>
+#include <cassert>
 
 namespace Xts {
-
-float
-LfoDSP::Generate()
-{
-  return 0.0f;
-}
 
 float
 LfoDSP::Next()
@@ -21,6 +17,20 @@ LfoDSP::Next()
 }
 
 float
+LfoDSP::Generate()
+{
+  float phase = static_cast<float>(_phase);
+  switch(_model->type)
+  {
+    case LfoType::Saw: return phase * 2.0f - 1.0f;
+		case LfoType::Sin: return sinf(phase * 2.0f * PI);
+		case LfoType::Sqr: return phase < 0.5f ? -1.0f : 1.0f;
+		case LfoType::Tri: return (phase < 0.5f ? phase : 1.0f - phase) * 4.0f - 1.0f;
+		default: assert(false); return 0.0f;
+	}
+}
+
+float
 LfoDSP::Freq(LfoModel const& model, SynthInput const& input)
 {
 	float length = model.sync ? SyncF(input, model.step) : TimeF(model.rate, input.rate);
@@ -30,6 +40,23 @@ LfoDSP::Freq(LfoModel const& model, SynthInput const& input)
 void
 LfoDSP::Plot(LfoModel const& model, PlotInput const& input, PlotOutput& output)
 {
+  float testRate = 1000.0f;
+	SynthInput testIn(testRate, input.bpm, 4, UnitNote::C);
+	output.freq = 0.0f;
+	output.rate = 0.0f;
+	output.clip = false;
+	output.bipolar = true;
+
+	if (!model.on) return;
+	output.freq = Freq(model, testIn);
+	output.rate = output.freq * input.pixels;
+
+	SynthState state;
+	SynthInput in(output.rate, input.bpm, 4, UnitNote::C);
+	LfoDSP dsp(&model, &in);
+	float samples = output.rate / output.freq;
+	for (int i = 0; i <= static_cast<int>(samples); i++)
+		output.samples->push_back(dsp.Next());
 }
 
 } // namespace Xts
