1b58862025d978d4136561b703cd392c5e86e0f1
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2022-01-23 22:18:49 +0100

    take 820

--- Src/Xt.Synth0.DSP/DSP/GlobalDSP.cpp
+++ Src/Xt.Synth0.DSP/DSP/GlobalDSP.cpp
@@ -6,15 +6,13 @@ namespace Xts {
 float
 GlobalDSP::Amp(SynthState const& state)
 {
-  //float amp = Level(_model->amp);
-  //float envAmt = Level(_model->ampEnvAmt);
   float lfoAmt = Level(_model->ampLfoAmt);
-  //float env = state.envs[static_cast<int>(_model->ampEnv)];
   float lfoVal = state.lfos[static_cast<int>(_model->ampLfo)];
-  return (1.0 - lfoAmt) + lfoAmt * lfoVal;
-  
-  // return amp * lfo * lfoAmt;
-  //return (amp * lfo * lfoAmt) + (1.0f - amp) * env * envAmt * lfo * lfoAmt;
+  float lfo = (1.0 - lfoAmt) + lfoAmt * lfoVal;
+  float amp = Level(_model->amp);
+  float envAmt = Level(_model->ampEnvAmt);
+  float env = state.envs[static_cast<int>(_model->ampEnv)];
+  return (amp * lfo) + (1.0f - amp) * env * envAmt * lfo;
 }
 
 } // namespace Xts
