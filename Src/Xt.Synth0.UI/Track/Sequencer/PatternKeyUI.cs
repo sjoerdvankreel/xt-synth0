9728594018651127434f4f7c621b111b687de261
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2022-01-29 22:40:40 +0100

    take 912

--- Src/Xt.Synth0.UI/Track/Sequencer/PatternKeyUI.cs
+++ Src/Xt.Synth0.UI/Track/Sequencer/PatternKeyUI.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,23 +10,43 @@ namespace Xt.Synth0.UI
 {
 	static class PatternKeyUI
 	{
-		const string NoteEditHint = $"Click + ./space/Q-U to edit";
+		const string NoteEditHint = $"Click + ./space/Z-M/Q-E to edit";
 
-		static readonly Key[] NoteKeys = new[]
+		static (int Oct, PatternNote Note) KeyToNoteEdit(Key key) => key switch
 		{
-			Key.OemPeriod, Key.Space,
-			Key.Q, Key.D2, Key.W, Key.D3, Key.E, Key.R,
-			Key.D5, Key.T, Key.D6, Key.Y, Key.D7, Key.U
+			Key.OemPeriod => (-1, PatternNote.None),
+			Key.Space => (-1, PatternNote.Off),
+			Key.Z => (0, PatternNote.C),
+			Key.S => (0, PatternNote.CSharp),
+			Key.X => (0, PatternNote.D),
+			Key.D => (0, PatternNote.DSharp),
+			Key.C => (0, PatternNote.E),
+			Key.V => (0, PatternNote.F),
+			Key.G => (0, PatternNote.FSharp),
+			Key.B => (0, PatternNote.G),
+			Key.H => (0, PatternNote.GSharp),
+			Key.N => (0, PatternNote.A),
+			Key.J => (0, PatternNote.ASharp),
+			Key.M => (0, PatternNote.B),
+			Key.Q => (1, PatternNote.C),
+			Key.D2 => (1, PatternNote.CSharp),
+			Key.W => (1, PatternNote.D),
+			Key.D3 => (1, PatternNote.DSharp),
+			Key.E => (1, PatternNote.E),
+			Key.R => (1, PatternNote.F),
+			Key.D5 => (1, PatternNote.FSharp),
+			Key.T => (1, PatternNote.G),
+			Key.D6 => (1, PatternNote.GSharp),
+			Key.Y => (1, PatternNote.A),
+			Key.D7 => (1, PatternNote.ASharp),
+			Key.U => (1, PatternNote.B),
+			Key.I => (2, PatternNote.C),
+			Key.D9 => (2, PatternNote.CSharp),
+			Key.O => (2, PatternNote.D),
+			Key.D0 => (2, PatternNote.DSharp),
+			Key.P => (2, PatternNote.E),
+			_ => (-2, PatternNote.None)
 		};
-
-		static void OnNoteKeyDown(Param param, KeyEventArgs e)
-		{
-			int note = new List<Key>(NoteKeys).IndexOf(e.Key);
-			if (note < 0) return;
-			e.Handled = true;
-			param.Value = note;
-			Utility.FocusDown();
-		}
 
 		static void OnOctTextInput(Param param, TextCompositionEventArgs e)
 		{
@@ -44,25 +63,38 @@ namespace Xt.Synth0.UI
 				interpolate();
 		}
 
+		static void OnNoteKeyDown(Param editOct, Param keyNote, Param keyOct, KeyEventArgs e)
+		{
+			var edit = KeyToNoteEdit(e.Key);
+			if (edit.Oct == -2) return;
+			e.Handled = true;
+			if (edit.Note == PatternNote.Off) keyNote.Value = (int)PatternNote.Off;
+			if (edit.Note == PatternNote.None) keyNote.Value = (int)PatternNote.None;
+			keyNote.Value = (int)edit.Note;
+			keyOct.Value = Math.Min(9, editOct.Value + edit.Oct);
+			Utility.FocusDown();
+		}
+
 		internal static void Add(Grid grid, AppModel app,
 			PatternKey key, int minKeys, int row, int col, Action interpolate)
 		{
 			var edit = app.Track.Seq.Edit;
-			grid.Add(MakeNote(app, key.Note, minKeys, row, col));
+			grid.Add(MakeNote(app, key.Note, key.Oct, minKeys, row, col));
 			grid.Add(MakeOct(app, key, minKeys, row, col + 1));
 			grid.Add(Create.Divider(new(row, col + 2), edit.Keys, minKeys));
 			grid.Add(MakeAmp(app, key, minKeys, row, col + 3, interpolate));
 		}
 
-		static UIElement MakeNote(AppModel app, Param note, int minKeys, int row, int col)
+		static UIElement MakeNote(AppModel app, Param keyNote,
+			Param keyOct, int minKeys, int row, int col)
 		{
 			var edit = app.Track.Seq.Edit;
 			var result = Create.PatternCell<TextBlock>(new(row, col));
-			result.ToolTip = string.Join("\n", note.Info.Description, NoteEditHint);
-			result.SetBinding(TextBlock.TextProperty, Bind.Format(note));
+			result.ToolTip = string.Join("\n", keyNote.Info.Description, NoteEditHint);
+			result.SetBinding(TextBlock.TextProperty, Bind.Format(keyNote));
 			result.SetBinding(TextBlock.ForegroundProperty, Bind.EnableRow(app, row));
 			result.SetBinding(UIElement.VisibilityProperty, Bind.Show(edit.Keys, minKeys));
-			result.KeyDown += (s, e) => OnNoteKeyDown(note, e);
+			result.KeyDown += (s, e) => OnNoteKeyDown(edit.Oct, keyNote, keyOct, e);
 			return result;
 		}
 
