47e683b4f769415536e006dce03ebf30351b823a
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2022-02-18 20:37:57 +0100

    take 1194

--- Src/Xt.Synth0.Model/Track/Synth/LfoModel.cs
+++ Src/Xt.Synth0.Model/Track/Synth/LfoModel.cs
@@ -45,8 +45,8 @@ namespace Xt.Synth0.Model
         static readonly IRelevance RelevanceTime = Relevance.When((LfoModel m) => m.Sync, (int s) => s == 0);
 
         static readonly ParamInfo OnInfo = ParamInfo.Toggle(p => &((Native*)p)->on, 0, nameof(On), nameof(On), "Enabled", false);
-        static readonly ParamInfo TypeInfo = ParamInfo.List<LfoType>(p => &((Native*)p)->type, 0, nameof(Type), nameof(Type), "Type");
-        static readonly ParamInfo PltyInfo = ParamInfo.List<LfoPolarity>(p => &((Native*)p)->plty, 0, nameof(Plty), "Polarity", "Polarity");
+        static readonly ParamInfo TypeInfo = ParamInfo.List<LfoType>(p => &((Native*)p)->type, 2, nameof(Type), nameof(Type), "Type");
+        static readonly ParamInfo PltyInfo = ParamInfo.List<LfoPolarity>(p => &((Native*)p)->plty, 2, nameof(Plty), "Polarity", "Polarity");
         static readonly ParamInfo SyncInfo = ParamInfo.Toggle(p => &((Native*)p)->sync, 1, nameof(Sync), nameof(Sync), "Sync to beat", false);
         static readonly ParamInfo StepInfo = ParamInfo.Step(p => &((Native*)p)->step, 1, nameof(Step), nameof(Step), "Rate steps", 1, 7, RelevanceSync);
         static readonly ParamInfo RateInfo = ParamInfo.Time(p => &((Native*)p)->rate, 1, nameof(Rate), nameof(Rate), "Rate milliseconds", 1, 26, RelevanceTime);
