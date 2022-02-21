712ef925d497ed799e4a4a8b1b0d184b2d4274b0
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2022-02-21 22:36:44 +0100

    take 1287

--- Src/Xt.Synth0.DSP/DSP/PlotDSP.cpp
+++ Src/Xt.Synth0.DSP/DSP/PlotDSP.cpp
@@ -12,6 +12,35 @@
 
 namespace Xts {
 
+static void
+SpectrumHSplits(std::vector<HSplit>& hSplits)
+{
+  hSplits.clear();
+  for (int oct = 0; oct < 12; oct++)
+  {
+    std::wstring marker = oct >= 2 ? std::to_wstring(oct - 2) : L"";
+    hSplits.emplace_back(HSplit(oct * 12, marker));
+  }
+  hSplits.emplace_back(HSplit(143, L""));
+}
+
+static void
+SpectrumVSplits(std::vector<VSplit>& vSplits)
+{
+  vSplits.clear();
+  for (int i = 0; i < 7; i++)
+  {
+    float split = 1.0f - 1.0f / (1 << i);
+    std::wstring marker = L"";
+    if (i == 0) marker = L"1";
+    if (i == 1) marker = std::wstring(1, UnicodeOneHalf);
+    if (i == 2) marker = std::wstring(1, UnicodeOneQuarter);
+    if (i == 3) marker = std::wstring(1, UnicodeOneEight);
+    vSplits.emplace_back(VSplit(split, marker));
+  }
+  vSplits.emplace_back(VSplit(1.0f, L"0"));
+}
+
 // https://stackoverflow.com/questions/604453/analyze-audio-using-fast-fourier-transform
 // https://dsp.stackexchange.com/questions/46692/calculating-1-3-octave-spectrum-from-fft-dft
 static float
@@ -32,7 +61,6 @@ Power(std::vector<std::complex<float>>& fft, float rate, int oct, int note)
 static void
 Spectrum(
   std::vector<float>& x, 
-  std::vector<HSplit>& hSplits,
   std::vector<VSplit>& vSplits,
   std::vector<std::complex<float>>& fft, 
   std::vector<std::complex<float>>& fftScratch,
@@ -44,16 +72,13 @@ Spectrum(
   Fft(x, fft, fftScratch);
 
   x.clear();
-  hSplits.clear();
   fft.erase(fft.begin() + fft.size() / 2, fft.end());  
   for(int oct = 0; oct < 12; oct++)
   {
     std::wstring marker = oct >= 2? std::to_wstring(oct - 2): L"";
-    hSplits.emplace_back(HSplit(i, marker));
     for(int note = 0; note < 12; note++, i++)
       x.push_back(Power(fft, rate, oct, note));
   }    
-  hSplits.emplace_back(HSplit(143, L""));
   for (size_t i = 0; i < x.size(); i++) max = std::max(x[i], max);
   for (size_t i = 0; i < x.size(); i++) x[i] = max == 0.0f? 0.0f: x[i] / max;
 
@@ -104,21 +129,21 @@ PlotDSP::Render(SynthModel const& model, PlotInput const& input, PlotOutput& out
     assert(false);
     break; }
   }  
-
   assert(output.rate <= input.rate);
+  
   if(!output.spec) return;  
-
   output.min = 0.0f;
   output.max = 1.0f;
-  if (output.lSamples->empty()) return;
-  output.lSamples->resize(NextPow2(output.lSamples->size()));
-  Spectrum(*output.lSamples, *output.hSplits, *output.vSplits, *output.fftData, *output.fftScratch, output.rate);
-  if(!output.stereo) return;
 
-  output.hSplits->clear();
-  output.vSplits->clear();
+  if (output.lSamples->empty()) return;  
+  output.lSamples->resize(NextPow2(output.lSamples->size()));
+  Spectrum(*output.lSamples, *output.vSplits, *output.fftData, *output.fftScratch, output.rate);
+  SpectrumHSplits(*output.hSplits);
+  SpectrumVSplits(*output.vSplits);
+  
+  if(!output.stereo) return;
   output.rSamples->resize(NextPow2(output.rSamples->size()));
-  Spectrum(*output.rSamples, *output.hSplits, *output.vSplits, *output.fftData, *output.fftScratch, output.rate);
+  Spectrum(*output.rSamples, *output.vSplits, *output.fftData, *output.fftScratch, output.rate);
 }
 
 } // namespace Xts
