ab2911baac226d0d8947fe2495a1d7c83a2cab51
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2022-03-01 20:27:46 +0100

    take 1300

--- Src/Xt.Synth0.DSP/DSP/PlotDSP.hpp
+++ Src/Xt.Synth0.DSP/DSP/PlotDSP.hpp
@@ -11,6 +11,9 @@
 #include <algorithm>
 #include <cassert>
 
+#define MIN_HOLD_MS 0.0f
+#define MAX_HOLD_MS 3000.0f
+
 namespace Xts {
 
 class PlotDSP
@@ -103,8 +106,8 @@ void PlotDSP::RenderStaged(
   output.stereo = (flags & PlotStereo) != 0;
   output.min = (flags & PlotBipolar) != 0 ? -1.0f : 0.0f;
   bool noResample = (flags & PlotNoResample) != 0;
-  float fhold = Param::TimeFramesF(hold, input.rate);
-  float releaseSamples = envModel.sync ? Param::StepFramesF(input.bpm, input.rate, envModel.rStp) : Param::TimeFramesF(envModel.r, input.rate);
+  float fhold = Param::TimeFramesF(hold, input.rate, MIN_HOLD_MS, MAX_HOLD_MS);
+  float releaseSamples = envModel.sync ? Param::StepFramesF(envModel.rStp, input.bpm, input.rate) : Param::TimeFramesF(envModel.r, input.rate, MIN_HOLD_MS, MAX_HOLD_MS);
   output.rate = output.spec || noResample ? input.rate : input.rate * input.pixels / (fhold + releaseSamples);
   fhold *= output.rate / input.rate;
   *output.vSplits = output.stereo? StereoVSPlits: (flags & PlotBipolar) != 0 ? BiVSPlits : UniVSPlits;
