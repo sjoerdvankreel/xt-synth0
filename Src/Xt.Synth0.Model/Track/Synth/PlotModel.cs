da0ff00ef1979fb647d7a3bc0bd63e3fbc64982f
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2022-03-04 22:12:44 +0100

    take 1338

--- Src/Xt.Synth0.Model/Track/Synth/PlotModel.cs
+++ Src/Xt.Synth0.Model/Track/Synth/PlotModel.cs
@@ -15,6 +15,7 @@ namespace Xt.Synth0.Model
 
     public unsafe sealed class PlotModel : IUIParamGroupModel
     {
+        const int DefaultHold = 100;
         const double MinHoldMs = 1.0;
         const double MaxHoldMs = 3000.0;
 
@@ -48,6 +49,6 @@ namespace Xt.Synth0.Model
         static readonly ParamInfo OnInfo = ParamInfo.Toggle(p => &((Native*)p)->on, 0, nameof(On), nameof(On), "Enabled", true);
         static readonly ParamInfo TypeInfo = ParamInfo.List<PlotType>(p => &((Native*)p)->type, 1, nameof(Type), nameof(Type), "Source");
         static readonly ParamInfo SpecInfo = ParamInfo.Toggle(p => &((Native*)p)->spec, 1, nameof(Spec), nameof(Spec), "Spectrum", false, RelevanceSpec);
-        static readonly ParamInfo HoldInfo = ParamInfo.Time(p => &((Native*)p)->hold, 1, nameof(Hold), nameof(Hold), "Hold key time", 0, MinHoldMs, MaxHoldMs, RelevanceHold);
+        static readonly ParamInfo HoldInfo = ParamInfo.Time(p => &((Native*)p)->hold, 1, nameof(Hold), nameof(Hold), "Hold key time", DefaultHold, MinHoldMs, MaxHoldMs, RelevanceHold);
     }
 }
