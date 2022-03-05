e2ba28180c956a34f75f7cc1aa315125125c5eab
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2022-03-05 14:11:26 +0100

    take 1346

---
+++ Src/Xt.Synth0.Model/Track/Synth/ModModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Xt.Synth0.Model
+{
+    unsafe class ModModel
+    {
+        [StructLayout(LayoutKind.Sequential, Pack = 8)]
+        internal ref struct Native
+        {
+            internal int target;
+            internal int amount;
+            internal int source;
+            internal int pad__;
+        };
+    }
+}
