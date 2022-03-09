5896934856faa0fa70ae5234e375192d3c244fd9
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2022-03-09 21:34:24 +0100

    take 1412

--- Src/Xt.Synth0.DSP/DSP/Synth/SynthDSP.cpp
+++ Src/Xt.Synth0.DSP/DSP/Synth/SynthDSP.cpp
@@ -12,17 +12,17 @@ SynthPlot::Params() const
   return result;
 }
 
-SynthDSP::
-SynthDSP(SynthModel const* model, int oct, UnitNote note, float velo, float bpm, float rate):
-_cv(&model->cv, velo, bpm, rate),
-_amp(&model->amp, velo),
-_audio(&model->audio, oct, note, rate) {}
-
 void
 SynthPlot::Render(SynthModel const& model, PlotInput const& input, PlotOutput& output)
 {
   auto plot = std::make_unique<SynthPlot>(&model);
   plot->RenderCore(input, model.plot.hold, output);
 }
+
+SynthDSP::
+SynthDSP(SynthModel const* model, int oct, UnitNote note, float velocity, float bpm, float rate):
+_cv(& model->cv, velocity, bpm, rate),
+_amp(&model->amp, velocity),
+_audio(&model->audio, oct, note, rate) {}
 
 } // namespace Xts
