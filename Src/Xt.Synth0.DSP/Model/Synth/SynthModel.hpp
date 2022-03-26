5d101f3de36e5674faf121b11fe4ea718ff73a8a
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2022-03-27 00:16:23 +0100

    Revert "Revert "TEMP? take 1564""
    This reverts commit 33161ab068e4e783939064b02c6f687c65b1d5e4.

--- Src/Xt.Synth0.DSP/Model/Synth/SynthModel.hpp
+++ Src/Xt.Synth0.DSP/Model/Synth/SynthModel.hpp
@@ -16,7 +16,7 @@ public:
   static ParamInfo const* Params();
   static void Init(ParamInfo* params, size_t count);
 };
-XTS_CHECK_SIZE(SynthModel, 928);
+XTS_CHECK_SIZE(SynthModel, 832);
 
 } // namespace Xts
 #endif // XTS_MODEL_SYNTH_SYNTH_MODEL_HPP
